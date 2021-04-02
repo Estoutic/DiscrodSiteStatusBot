20d83e4ad365b6b16f04a066ccef9b1604b56fda
Ilya Udalov <ilya_udalov_04@mail.ru>
2021-04-02 15:39:45 +0300

    add new functions

---
+++ config.py
@@ -0,0 +1,6 @@
+settings = {
+    'token': 'ODIwMDQ0OTczOTMxNDk1NTA1.YEvcZQ.lO-5ZuuBtg_v1YqryUSJ6KbAkjM',
+    'bot': 'Sitetatus',
+    'id': 820044973931495505,
+    'prefix': '!'
+}
---
+++ resource.py
@@ -0,0 +1,11 @@
+class Resurce():
+
+    def __init___(self, name, url):
+        self.name = name
+        self.url = url
+
+    def name(self):
+        return name
+
+    def url(self):
+        return url
